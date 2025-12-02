61d4d1f235963aa9bc78b044148315c2d946d14a
miaoda <miaoda@baidu.com>
2025-12-02 21:00:48 +0100

    chore: commit tina generated files

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -2,7 +2,7 @@ import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
 export const client = createClient({
   cacheDir:
-    "/Users/andreasnahrgang/digitaloriginal.store_NEW/app-7uja2c23lczl/tina/__generated__/.cache/1764656890011",
+    "/Users/andreasnahrgang/digitaloriginal.store_NEW/app-7uja2c23lczl/tina/__generated__/.cache/1764705610596",
   url: "http://localhost:4001/graphql",
   token: "dummy-token",
   queries,
